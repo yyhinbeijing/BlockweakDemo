29b9db7b1052e62c4d0247e32d170fcec673014d
zsling <zsling@exutech.cn>
2016-03-15 20:55:06 +0800

    Initial Commit

---
+++ BlockweakDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockweakDemo
+//
+//  Created by summer on 16/3/15.
+//  Copyright © 2016年 cn.exutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

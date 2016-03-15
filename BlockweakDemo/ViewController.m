90e5f612b8541261d5daf8e6032025a3c1966160
zsling <zsling@exutech.cn>
2016-03-15 23:04:15 +0800

    block  demo
    block  demo

--- BlockweakDemo/ViewController.m
+++ BlockweakDemo/ViewController.m
@@ -16,6 +16,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    __block int multiplier = 7;
+    int (^myBlock)(int) = ^(int num) {
+        multiplier ++;
+        return num * multiplier;
+    };
+    NSLog(@"%d",myBlock(5));
+    
+    NSMutableArray *mArray = [NSMutableArray arrayWithObjects:@"a",@"b",@"abc",nil];
+    NSMutableArray *mArrayCount = [NSMutableArray arrayWithCapacity:1];
+    
+    [mArray enumerateObjectsWithOptions:NSEnumerationConcurrent usingBlock:^(id obj,NSUInteger idx,BOOL *stop){
+        [mArrayCount addObject:[NSNumber numberWithInt:[obj length]]];
+    }];
+    NSLog(@"%@",mArrayCount);
+    
+    __block int a = 1;//如何我们
+    int (^backValue)(int) = ^(int num) {
+        return num + a;
+    };
+    
+    a += 1;
+    
+    int b = backValue(1);
+    NSLog(@"b-->%d",b);
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 300, 300, 40)];
+    label.text = @"block作用域赋值";
+    void (^backddValue)(NSString *) = ^(NSString *str) {
+        label.text = str;
+    };
+    backddValue(@"block作用域内赋值");
+    [self.view addSubview:label];
+    
+    label = [[UILabel alloc] init];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
